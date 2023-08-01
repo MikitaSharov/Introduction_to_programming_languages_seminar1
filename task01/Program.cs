237f26e8caedb38a69b17f7a3fa58aaa6fdcb57b
Nikita Sharov <lliapik@gmail.com>
2023-08-01 12:13:25 +0300

    seminar1 task01,03,05

---
+++ task01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+# Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+11:05
+Console.WriteLine(result);
+*/
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1 * number1)
+{
+    Console.Write("правда");
+}
+else
+{
+    Console.Write("ложь");
+}
---
+++ task03/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+03. # Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+11:38
+3 -> Среда
+5 -> Пятница
+
+11:38
+*/
+
+Console.WriteLine("Введите день недели от 1 до 7: ");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+if (numberDay == 1)
+{
+    Console.Write("понедельник");
+}
+else if (numberDay == 2)
+{
+    Console.Write("вторник");
+}
+else if (numberDay == 3)
+{
+    Console.Write("среда");
+}
+else if (numberDay == 4)
+{
+    Console.Write("четверг");
+}
+else if (numberDay == 5)
+{
+    Console.Write("пятница");
+}
+else if (numberDay == 6)
+{
+    Console.Write("суббота");
+}
+else if (numberDay == 7)
+{
+    Console.Write("воскресенье");
+}
+else
+{
+    Console.Write("число от 1 до 7 НАДО");
+}
