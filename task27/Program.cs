a67960f2347fd5abc8f488c595f37af986f2b16b
Nikita Sharov <lliapik@gmail.com>
2023-08-24 12:58:11 +0300

    SumDigits

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Enter number: ");
+Console.WriteLine(SumDigits(Convert.ToInt32(Console.ReadLine())));
+
+int SumDigits(int number)
+{
+    int sumDigits = 0;
+    while(number > 0)
+    {
+        sumDigits = sumDigits + number%10;
+        number/=10;
+    }
+
+    return sumDigits;
+}
