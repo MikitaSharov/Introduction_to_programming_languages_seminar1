ed6a8c8d54d80eff314f31a3485f412e66d05209
Nikita Sharov <lliapik@gmail.com>
2023-09-01 15:42:14 +0300

    bool

--- task40/Program.cs
+++ task40/Program.cs
@@ -1,32 +1,25 @@
 ﻿/* Напишите программу, которая принимает на вход три числа и проверяет,
 может ли существовать треугольник с сторонами такой длины. */
 
-Console.Write("Введите длины сторон: ");
-
-
 int[] FillUserArray()
 {
     int[] array = new int[3];
-    for (int i=0; i<3; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        array [i] = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Введите длину {i + 1} стороны: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
     return array;
 }
 
 bool TriangleExistance(int[] array)
 {
-    if (array[0] < array[1]+array[2] 
-    && array[1] < array[0]+array[2]
-    && array[2] < array[0]+array[1])
-    {
-        return true;
-    }
-    else
-    return false;
+    if (array[0] < array[1] + array[2]
+    && array[1] < array[0] + array[2]
+    && array[2] < array[0] + array[1]) return true;
+    else return false;
 
 }
 
 int[] userArray = FillUserArray();
-bool result=TriangleExistance(userArray);
-Console.Write(result);
+Console.Write(TriangleExistance(userArray));
