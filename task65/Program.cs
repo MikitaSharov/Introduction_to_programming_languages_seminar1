20d63f3d30a4019ee6404096334656db63e32a22
Nikita Sharov <lliapik@gmail.com>
2023-09-12 11:30:41 +0300

    recursion PrintMToN

---
+++ task65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Write("Введите число: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int start, int finish)
+{
+    if (start <= finish)
+    {
+        Console.Write(start + " ");
+        PrintNumbers(start + 1, finish);
+    }
+}
+
+PrintNumbers(userNumberM, userNumberN);
