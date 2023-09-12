ca4c75799af26946c9a62b72444d53d38fa284a9
Nikita Sharov <lliapik@gmail.com>
2023-09-12 09:55:01 +0300

    recursion SumMToN

---
+++ task66/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int userNumberM = 4;
+int userNumberN = 8;
+int sum = SumMToN(userNumberM, userNumberN);
+Console.WriteLine(sum);
+
+int SumMToN(int numberM, int numberN)
+{
+    if (numberM == numberN) return numberM;
+    else return numberM + SumMToN(numberM + 1, numberN);
+}
