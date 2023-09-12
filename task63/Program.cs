b18726d16e3f78dffc132b1415f576d1631a8c1c
Nikita Sharov <lliapik@gmail.com>
2023-09-12 11:25:43 +0300

    recursion Print1ToN

---
+++ task63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int number, int start = 1)
+{
+    if (start <= number)
+    {
+        Console.Write(start + " ");
+        PrintNumbers(number, start + 1);
+    }
+}
+
+PrintNumbers(userNumber);
